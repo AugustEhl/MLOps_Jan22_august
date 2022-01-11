7313649f2b517bd04fea4638713795cc56f68356
August Ehlers <august.ehlers@gmail.com>
2022-01-11 22:35:39 +0100

    Think it works now

--- Day_5/m15/tests/test_training.py
+++ Day_5/m15/tests/test_training.py
@@ -1,6 +1,8 @@
 from tests import _PATH_TRAINING
+from tests import _PATH_DATA
 import sys
 sys.path.append(_PATH_TRAINING)
+sys.path.append(_PATH_DATA)
 from training import num_epochs, lr, batch_size, weight_decay
 
 assert num_epochs != None
